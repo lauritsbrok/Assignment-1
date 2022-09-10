5ffb11d0848cf0db34be3b8887b4f329eb495536
Laurits Brok <lauritsbrok@gmail.com>
2022-09-10 18:05:01 +0200

    Created methods and test cases for Flatten(), Filter(), InnerText() and Urls()

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,5 +1,55 @@
+using Xunit;
+
 namespace Assignment1.Tests;
 
-public class RegExprTests
-{
+public class RegExprTests {
+    [Fact]
+    public void getInnerHtmlFromATag()
+    {
+        // Arrange
+        var html = "<div>\n<p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p>\n</div>";
+        var tag = "a";
+
+        // Act
+        var listOfInnerHtml = RegExpr.InnerText(html, tag);
+
+        // Assert
+        Assert.Equal(new List<String> {"theoretical computer science", "formal language", "characters", "pattern", "string searching algorithms", "strings"}, listOfInnerHtml);
+    }
+
+    [Fact]
+    public void getInnerHtmlFromPTagWithNesting()
+    {
+        // Arrange
+        var html = "<div><a>The phrase <i>regular expressions</i> (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing <u>patterns</u> that matching <em>text</em> need to conform to.</a></div>";
+        var tag = "p";
+
+        // Act
+        var listOfInnerHtml = RegExpr.InnerText(html, tag);
+
+        // Assert
+        Assert.Equal(new List<String> {"The phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to."}, listOfInnerHtml);
+    }
+
+    [Fact]
+    public void getURLandTitle()
+    {
+        // Arrange
+        var html = "<div>\n<p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p>\n</div>";
+
+        // Act
+        var listOfURLandTitles = RegExpr.Urls(html);
+        var expectedResults = new List <(Uri, String)> {
+            (new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science"), 
+            (new Uri("https://en.wikipedia.org/wiki/Formal_language"), "Formal language"),
+            (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"), "Character (computing)"),
+            (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"), "Pattern matching"),
+            (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"),
+            (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)"),
+
+        };
+
+        // Assert
+        Assert.Equal(expectedResults, listOfURLandTitles);
+    }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Assignment1;
 
 public static class RegExpr
@@ -6,5 +8,22 @@ public static class RegExpr
 
     public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
 
-    public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+    // Regex: (?<=">|a>)\w.*?(?=<\/a>) where "a" is a variable
+    public static IEnumerable<string> InnerText(string html, string tag) {
+        String pattern = "(?<=\">|a>)\\w.*?(?=<\\/a>)";
+        String replacePattern = "<\\w+>|<\\/\\w+>";
+        MatchCollection matches = Regex.Matches(html, pattern);
+        foreach(Match match in matches){
+            var replacedMatch = Regex.Replace(match.ToString(), replacePattern, "");
+            yield return replacedMatch.ToString();
+        }
+    }
+
+    public static IEnumerable<(Uri url, string title)> Urls(string html){
+        String pattern = "(?<=href=\"|title=\").*?(?=\")";
+        MatchCollection matches = Regex.Matches(html, pattern);
+        for(int i = 0; i < matches.Count(); i = i+2){
+            yield return (new Uri(matches[i].ToString()), matches[i+1].ToString());
+        }
+    }
 }
