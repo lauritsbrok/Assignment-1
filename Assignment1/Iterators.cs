3aa56061711558d71715d6c0097e197c7b9ca76a
thekure <laku@itu.dk>
2022-09-10 09:56:04 +0200

    finished regex for resolutions

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -2,7 +2,9 @@ namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items){
+        throw new NotImplementedException();
+    }
 
     public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,10 +1,27 @@
+using System.Text.RegularExpressions;
+using static System.Console;
+
 namespace Assignment1;
 
 public static class RegExpr
 {
     public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions){
+        var pattern = @"^(?<width>\d{3,4})x(?<height>\d{3,4})$";
+        var rg = new Regex(pattern);
+
+        foreach(string s in resolutions){
+            string[] words = s.Split(',');
+            foreach (var word in words){
+                var trimmed = word.Trim();
+                var match   = Regex.Match(trimmed, pattern);
+                var width   = int.Parse(match.Groups["width"].Value);
+                var height  = int.Parse(match.Groups["height"].Value);
+                yield return (width, height);
+            }
+        }
+    }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
